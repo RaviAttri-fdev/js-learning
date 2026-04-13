df204974dd081cc8893a5e3b92f8547136d2e565
RaviAttri-fdev <ravi@techarchsoftwares.in>
2026-04-13 10:06:24 +0000

    datatypes

---
+++ .devcontainer/basics_js/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict" // treat all JS code as newer version
+
+// alert(5 + 3) // we are using nodejs, not browser
+
+let name = "takes"
+let age = 20
+let isLoggedIn = false
+
+//Premitive datatyoes
+// number => 2 to power of 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assign
+// symbol => unique
+
+console.log(typeof null);
