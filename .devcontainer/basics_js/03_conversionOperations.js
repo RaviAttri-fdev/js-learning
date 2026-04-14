e40fed43acef8fd087746ac6ea78c4cdd124f79f
RaviAttri-fdev <ravi@techarchsoftwares.in>
2026-04-14 11:40:03 +0000

    conversion file

--- .devcontainer/basics_js/03_conversionOperations.js
+++ .devcontainer/basics_js/03_conversionOperations.js
@@ -12,10 +12,10 @@ let valueInNumber = Number(score)
 // "33abc" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = "hitesh"
+let isLoggedIn = "ravi"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-//  console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -24,13 +24,13 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
 
 // *********************** Operations ***********************
 
-let value = 3
-let negValue = -value
+//let value = 3
+//let negValue = -value
 // console.log(negValue);
 
 // console.log(2+2);
@@ -40,10 +40,10 @@ let negValue = -value
 // console.log(2/3);
 // console.log(2%3);
 
-let str1 = "hello"
-let str2 = " hitesh"
+//let str1 = "hello"
+//let str2 = " hitesh"
 
-let str3 = str1 + str2
+//let str3 = str1 + str2
 // console.log(str3);
 
 // console.log("1" + 2);
@@ -56,13 +56,13 @@ let str3 = str1 + str2
 // console.log(+true);
 // console.log(+"");
 
-let num1, num2, num3
+//let num1, num2, num3
 
-num1 = num2 = num3 = 2 + 2
+//num1 = num2 = num3 = 2 + 2
 
-let gameCounter = 100
-++gameCounter;
-console.log(gameCounter);
+//let gameCounter = 100
+//++gameCounter;
+//console.log(gameCounter);
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
