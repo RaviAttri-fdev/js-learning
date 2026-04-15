05842c82392964fab2453a3c08be4c31c3f75ee6
RaviAttri-fdev <ravi@techarchsoftwares.in>
2026-04-15 06:52:51 +0000

    changes to datatypes summary

--- .devcontainer/basics_js/datatypes-summary.js
+++ .devcontainer/basics_js/datatypes-summary.js
@@ -35,3 +35,29 @@ const myFunction = function(){
 console.log(typeof heros);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// Stack (Premitive) Meet a copy original value not changed , Heap memory (Non -premitive) meet a reference (changes to the original value)
+
+
+let myYoutubename = "rivsdotcom"
+
+let anothrname = myYoutubename
+
+anothrname = "coffee aur code"
+
+console.log(myYoutubename);
+console.log(anothrname);
+
+let userOne = {
+    email: "user.google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "ravi@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
