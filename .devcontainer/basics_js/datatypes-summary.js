4cf4bc53aacfab23039473df88474a6fa09a5934
RaviAttri-fdev <ravi@techarchsoftwares.in>
2026-04-15 06:37:53 +0000

    done

--- .devcontainer/basics_js/datatypes-summary.js
+++ .devcontainer/basics_js/datatypes-summary.js
@@ -14,7 +14,7 @@ const anotherId = Symbol('123')
 
 console.log(id === anotherId);
 
-// const bigNumber = 3456543576654356754n
+ const bigNumber = 3456543576654356754n
 
 
 
@@ -32,6 +32,6 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+console.log(typeof heros);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
