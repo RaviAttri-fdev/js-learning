793de71ec74f8578c1fb63029759a359f439a522
RaviAttri-fdev <ravi@techarchsoftwares.in>
2026-05-15 18:18:12 +0530

    chnages

--- js-learning-main/.devcontainer/03_basics/01_functions.js
+++ js-learning-main/.devcontainer/03_basics/01_functions.js
@@ -2,7 +2,13 @@ function myName (){
     console.log("R");
     console.log("a");
     console.log("v");
-
+    console.log("i");
+    console.log("i");
+    console.log("i");
+    console.log("i");
+    console.log("i");
+    console.log("i");
+    console.log("i");
 }
 
 myName()
