6834c3c420e2f023fb259d45216e4e7515c1e369
RaviAttri-fdev <ravi@techarchsoftwares.in>
2026-05-18 18:27:20 +0530

    chnages

--- js-learning-main/.devcontainer/03_basics/02_scopes.js
+++ js-learning-main/.devcontainer/03_basics/02_scopes.js
@@ -16,12 +16,52 @@ let a = 300
 if (true){
     let a = 10
     const b = 20
-    console.log("Inner:", a);
-    
+    //console.log("Inner:", a);
 }
-console.log("Outside:", a);
+//console.log("Outside:", a);
  //console.log(b);
  //console.log(c);
 
 
  
+//nested scope : child can access parent value but parent cannot access child value
+
+function one () {
+    const username = "ravi"
+    function two(){
+        const website = "youtube"
+       // console.log(username);
+    }
+    //console.log(website);
+    
+    two()
+}
+
+//one ()
+
+if (true){
+    const username = "ravi"
+    if(username === "ravi"){
+        const website = " youtube"
+    //  console.log(username + website);
+    }
+    //console.log(website);
+    
+}
+
+//console.log(username);
+
+
+// +++++++++++ important +++++ hueisting
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+
+addTwo(5)   // cannot declare because holding value to const
+const addTwo = function(num){
+    return num + 2
+}
+
