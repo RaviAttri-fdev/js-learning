0c58a5d928251fe9668095e48adb4c16948b4f36
RaviAttri-fdev <ravi@techarchsoftwares.in>
2026-05-18 13:03:33 +0530

    first commit

--- js-learning-main/.devcontainer/03_basics/01_functions.js
+++ js-learning-main/.devcontainer/03_basics/01_functions.js
@@ -7,9 +7,32 @@ function myName (){
 
 //myName()
 
+// function addTwoNumbers(number1, number2){   //number1 , number2 are parameters
+//  console.log( number1 + number2 );
+// }
+
+// const result = addTwoNumbers(4, 6)  // 4,6 are arguments
+
+// console.log("result: ", result);
+
 function addTwoNumbers(number1, number2){   //number1 , number2 are parameters
- console.log( number1 + number2 );
+    // let result = number1 + number2
+    // return result
+    // console.log("Ravi");  after return never print, not execute
+    return number1 + number2
 }
 
-addTwoNumbers(4, 6)  // 4,6 are arguments
+const result = addTwoNumbers(4, 6)  // 4,6 are arguments
 
+// console.log("result: ", result);
+
+function loginUserMessage(username = "sandy"){
+    if(username === undefined){   // or  if(!username){ her ! is same as ===
+        console.log("Please enter a user name");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("ravi"))
+console.log(loginUserMessage())
